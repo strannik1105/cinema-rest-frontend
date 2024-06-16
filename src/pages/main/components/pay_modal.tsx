31a91b243f46d8e03d880805a8b10da152860fa5
Daniil325 <potekhindaniill@mail.ru>
2024-06-16 20:15:41 +0700

    fix: default values for fio and email

--- src/pages/main/components/pay_modal.tsx
+++ src/pages/main/components/pay_modal.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 
 export const PayModal = () => {
@@ -10,26 +11,46 @@ export const PayModal = () => {
     // @ts-ignore
     const totalPrice = JSON.parse(localStorage.getItem("totalPrice"))
 
+    // @ts-ignore
+    const userSid = localStorage.getItem("user_sid")
+    console.log(userSid)
+
+
+    const [user, setUser] = useState<any>();
+
+    useEffect(() => {
+        axios.get("http://localhost:8001/api/v1/users/" + userSid)
+            .then(data => {
+                console.log(data.data)
+                setUser(() => data.data)
+            })
+    }, []);
+
 
     const navigate = useNavigate();
     const onPay = () => {
         navigate("/blist")
     }
 
-    return (
-        <form className="payform-tinkoff" name="payform-tinkoff">
-            <input className="payform-tinkoff-row" type="hidden" name="terminalkey" value="TinkoffBankTest"/>
-            <input className="payform-tinkoff-row" type="hidden" name="frame" value="false"/>
-            <input className="payform-tinkoff-row" type="hidden" name="language" value="ru"/>
-            <input className="payform-tinkoff-row" type="text" placeholder="Сумма заказа" name="amount" value={totalPrice}
-                   required disabled/>
-            <input className="payform-tinkoff-row" type="hidden" placeholder="Номер заказа" name="order"/>
-            <input className="payform-tinkoff-row" type="text" placeholder="ФИО плательщика"
-                   name="name"/>
-            <input className="payform-tinkoff-row" type="email" placeholder="E-mail"
-                   name="email"/>
-            <input className="payform-tinkoff-row payform-tinkoff-btn" type="submit"
-                   value="Оплатить" onClick={onPay}/>
-        </form>
-    )
+    if (user) {
+        return (
+            <form className="payform-tinkoff" name="payform-tinkoff">
+                <input className="payform-tinkoff-row" type="hidden" name="terminalkey" value="TinkoffBankTest"/>
+                <input className="payform-tinkoff-row" type="hidden" name="frame" value="false"/>
+                <input className="payform-tinkoff-row" type="hidden" name="language" value="ru"/>
+                <input className="payform-tinkoff-row" type="text" placeholder="Сумма заказа" name="amount"
+                       value={totalPrice}
+                       required disabled/>
+                <input className="payform-tinkoff-row" type="hidden" placeholder="Номер заказа" name="order"/>
+                <input className="payform-tinkoff-row" type="text" placeholder="ФИО плательщика"
+                       name="name" defaultValue={a.user}/>
+                <input className="payform-tinkoff-row" type="email" placeholder="E-mail"
+                       name="email" defaultValue={user.email}/>
+                <input className="payform-tinkoff-row payform-tinkoff-btn" type="submit"
+                       value="Оплатить" onClick={onPay}/>
+            </form>
+        )
+    }
+    return <form className="payform-tinkoff" name="payform-tinkoff"></form>
+
 }
